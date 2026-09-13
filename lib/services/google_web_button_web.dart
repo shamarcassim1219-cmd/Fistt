69ee8490fd0c7a12b3629573f90cca14b0aa97c4
shamarcassim1219-cmd <helpmybass@gmail.com>
2026-09-13 18:47:39 +0530

    fix: remove invalid GSIButtonShape.circle causing web build failure

--- lib/services/google_web_button_web.dart
+++ lib/services/google_web_button_web.dart
@@ -4,9 +4,8 @@ import 'package:google_sign_in_web/google_sign_in_web.dart' as web;
 
 Widget buildGoogleWebButton() {
   return (GoogleSignInPlatform.instance as web.GoogleSignInPlugin).renderButton(
-    configuration: const web.GSIButtonConfiguration(
+    configuration: web.GSIButtonConfiguration(
       type: web.GSIButtonType.icon,
-      shape: web.GSIButtonShape.circle,
       theme: web.GSIButtonTheme.filledBlue,
       size: web.GSIButtonSize.large,
     ),
