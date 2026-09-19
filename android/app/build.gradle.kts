cc623448ad72dd48d929e327609074ea34cccc7f
shamarcassim1219-cmd <helpmybass@gmail.com>
2026-09-19 11:12:30 +0530

    release: v1.0.84 - fix applicationId, Google Sign-In, launcher icon

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -17,7 +18,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.mygame_app"
+        applicationId = "com.mygame.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
